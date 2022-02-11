720b1056eb7de78768a0cffb68fc502a38e125df
Michael Ingersoll <mingersoll@uwm.com>
2022-02-11 09:06:26 -0500

    Add project files.

---
+++ UWMAMPAS.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace UWMAMPAS.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
