dccf0d1e835c75f266a10ef59f93b43aac0e0939
Michael Ingersoll <mingersoll@bluecraft.onmicrosoft.com>
2022-02-27 11:30:25 -0500

    Created Database Migrations

---
+++ UWMAMPAS.Data/Migrations/20220227152500_InitialCreate.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UWMAMPAS.Data.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Movies",
+                columns: table => new
+                {
+                    MovieId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Director = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Movies", x => x.MovieId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Performers",
+                columns: table => new
+                {
+                    PerformerId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    MovieId = table.Column<Guid>(type: "uniqueidentifier", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Performers", x => x.PerformerId);
+                    table.ForeignKey(
+                        name: "FK_Performers_Movies_MovieId",
+                        column: x => x.MovieId,
+                        principalTable: "Movies",
+                        principalColumn: "MovieId");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Performers_MovieId",
+                table: "Performers",
+                column: "MovieId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Performers");
+
+            migrationBuilder.DropTable(
+                name: "Movies");
+        }
+    }
+}
